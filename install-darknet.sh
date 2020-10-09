1b3ca5535f04baa3353b79c272e01c57c8b5f28d
Micheleen Harris <michhar@microsoft.com>
2020-10-09 15:18:18 -0700

    Clean up and fix driver install

--- install-darknet.sh
+++ install-darknet.sh
@@ -26,42 +26,30 @@ fi
 # Set permissions so we can write to log
 sudo chmod ugo+rw install-log.txt
 
+
+export DEBIAN_FRONTEND=noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
 # Ubuntu 18.04, CUDA 10.1, libcudnn 7.5.1 and NVIDIA 418.67 drivers
 # https://askubuntu.com/questions/1077061/how-do-i-install-nvidia-and-cuda-drivers-into-ubuntu
 sudo rm /etc/apt/sources.list.d/cuda*
-sudo apt -y remove --autoremove nvidia-cuda-toolkit
-sudo apt -y remove --autoremove nvidia-*
-sudo apt -y update
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt -y update
-sudo apt -y install nvidia-driver-440
+sudo apt remove -y --autoremove nvidia-cuda-toolkit
+sudo apt remove -y --autoremove nvidia-*
+sudo apt update
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
+sudo apt update
+#sudo apt install -y -q cuda-drivers-455
 sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
 sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
-sudo apt -y update
-sudo apt -y install cuda-10-1 2>> install-log.txt
-sudo apt -y install libcudnn7 2>> install-log.txt
-
-# if [ ! -d /usr/local/cuda ]; then
-#     echo "Installing CUDA and drivers..." >> install-log.txt
-#     # CUDA 11.0 install
-#     # https://www.howtoforge.com/tutorial/how-to-install-nvidia-cuda-on-ubuntu-1804/
-#     sudo wget https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda_11.0.3_450.51.06_linux.run
-#     sudo chmod +x cuda_11.0.3_450.51.06_linux.run
-#     sudo ./cuda_11.0.3_450.51.06_linux.run --silent --driver --toolkit --samples 2>> install-log.txt
-# else
-#     echo "CUDA installed already" >> install-log.txt
-# fi
-
-# # cuDNN 7 install
-# sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
-# sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-# sudo apt update && sudo apt install libcudnn7
+sudo apt update
+sudo apt install -y -q cuda-drivers cuda-demo-suite-11-0 cuda-runtime-11-0 cuda-11-0
+sudo apt install -y -q libcudnn7
 
 echo "Installing OpenCV..." >> install-log.txt
 
 # Install OpenCV
-sudo apt -y update && sudo apt -y install libopencv-dev python3-opencv 2>> install-log.txt
+sudo apt update && sudo apt install -y libopencv-dev python3-opencv 2>> install-log.txt
 
 # Clone darknet
 cd $WD
@@ -70,8 +58,8 @@ cd darknet/
 
 # Update variables to enable GPU acceleration for build
 sed -i "s/GPU=0/GPU=1/g" Makefile
-#sed -i "s/CUDNN=0/CUDNN=1/g" Makefile
-#sed -i "s/CUDNN_HALF=0/CUDNN_HALF=1/g" Makefile
+# sed -i "s/CUDNN=0/CUDNN=1/g" Makefile
+# sed -i "s/CUDNN_HALF=0/CUDNN_HALF=1/g" Makefile
 sed -i "s/OPENCV=0/OPENCV=1/g" Makefile
 sed -i "s/AVX=0/AVX=1/g" Makefile
 sed -i "s/OPENMP=0/OPENMP=1/g" Makefile
@@ -100,3 +88,4 @@ cd $WD
 sudo chmod -R ugo+rw darknet/
 
 echo "Done building darknet!" >> install-log.txt
+
