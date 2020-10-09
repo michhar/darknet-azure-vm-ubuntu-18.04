e755d9c542b1d9fa1f9e6255a2a7f5d95d4f4673
Micheleen Harris <michhar@microsoft.com>
2020-10-09 13:55:11 -0700

    Alternative cuda, drivers and cudnn install

--- install-darknet.sh
+++ install-darknet.sh
@@ -26,21 +26,37 @@ fi
 # Set permissions so we can write to log
 sudo chmod ugo+rw install-log.txt
 
-if [ ! -d /usr/local/cuda ]; then
-    echo "Installing CUDA and drivers..." >> install-log.txt
-    # CUDA 11.0 install
-    # https://www.howtoforge.com/tutorial/how-to-install-nvidia-cuda-on-ubuntu-1804/
-    sudo wget https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda_11.0.3_450.51.06_linux.run
-    sudo chmod +x cuda_11.0.3_450.51.06_linux.run
-    sudo ./cuda_11.0.3_450.51.06_linux.run --silent --driver --toolkit --samples 2>> install-log.txt
-else
-    echo "CUDA installed already" >> install-log.txt
-fi
-
-# cuDNN 7 install
-sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+# Ubuntu 18.04, CUDA 10.1, libcudnn 7.5.1 and NVIDIA 418.67 drivers
+# https://askubuntu.com/questions/1077061/how-do-i-install-nvidia-and-cuda-drivers-into-ubuntu
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt -y remove --autoremove nvidia-cuda-toolkit
+sudo apt -y remove --autoremove nvidia-*
+sudo apt -y update
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt -y update
+sudo apt -y install nvidia-driver-440
 sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo apt update && sudo apt install libcudnn7
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+sudo apt -y update
+sudo apt -y install cuda-10-1 2>> install-log.txt
+sudo apt -y install libcudnn7 2>> install-log.txt
+
+# if [ ! -d /usr/local/cuda ]; then
+#     echo "Installing CUDA and drivers..." >> install-log.txt
+#     # CUDA 11.0 install
+#     # https://www.howtoforge.com/tutorial/how-to-install-nvidia-cuda-on-ubuntu-1804/
+#     sudo wget https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda_11.0.3_450.51.06_linux.run
+#     sudo chmod +x cuda_11.0.3_450.51.06_linux.run
+#     sudo ./cuda_11.0.3_450.51.06_linux.run --silent --driver --toolkit --samples 2>> install-log.txt
+# else
+#     echo "CUDA installed already" >> install-log.txt
+# fi
+
+# # cuDNN 7 install
+# sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+# sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+# sudo apt update && sudo apt install libcudnn7
 
 echo "Installing OpenCV..." >> install-log.txt
 
